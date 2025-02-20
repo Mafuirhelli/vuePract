214195c25d26949ca8591f02d1d4f956145dafe0
Mafuirhelli <61899009+Mafuirhelli@users.noreply.github.com>
2025-02-20 12:27:26 +0700

    home commit

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 10,
+        onStock: true,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral',],
         variants: [
